b38e353f2b4a557535522e2e3eebc50fad907e76
George <georgeneves.dev@gmail.com>
2023-09-21 22:53:36 -0300

    version 2

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -1,37 +1,27 @@
 package app;
 
+import util.Calculator;
+
 import java.util.Scanner;
 import java.util.Locale;
 
-/**
- * Hello world!
- *
- */
 public class Program {
-
-    public static final double PI = 3.14159;
     public static void main( String[] args ) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circunference(radius);
+        double c = calculator.circunference(radius);
 
-        double v = volume(radius);
+        double v = calculator.volume(radius);
 
         System.out.printf("Circunference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
         sc.close();
-    }
-
-    public static double circunference(double radius){
-        return 2.0 * PI * radius;
-    }
-    public static double volume(double radius){
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
